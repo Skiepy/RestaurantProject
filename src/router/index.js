b6cb5dc262f184e170d50a6b16b81fa80cb0a9db
Skiepy <72888827+Skiepy@users.noreply.github.com>
2022-11-07 16:12:08 +0100

    Squelette Home / ajout login & register / modifs fichiers

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+
+    component: () => import('../views/RegisterView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+
+    component: () => import('../views/LoginView.vue')
   }
 ]
 
