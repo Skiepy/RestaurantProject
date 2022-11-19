5278de10e231c7b2ebe341e903a426d470bfbcb0
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2022-11-20 01:39:42 +0200

    Update MenuView.vue

--- Frontend/src/views/MenuView.vue
+++ Frontend/src/views/MenuView.vue
@@ -1,5 +1,51 @@
 <template>
     <div class="about">
-        <h1>This is the menu page</h1>
+        <h1>Today's Menu</h1>
+        <div class="plats">
+        <p class="titre">FIRST COURSE</p>
+        <p class="manger">Canard Laqué</p>
+        </div>
+        <div class="plats">
+        <p class="titre">MAIN COURSE</p>
+        <p class="manger">Canard Laqué</p>
+        </div>
+        <div class="plats">
+        <p class="titre">DESERT</p>
+        <p class="manger">Canard Laqué</p>
+        </div>
     </div>
 </template>
+
+<style>
+@import url('https://fonts.cdnfonts.com/css/papyrus');
+
+.about{
+    border-width:1px;
+    border-style:outset;
+    border-color: #dbb172;  
+}
+
+h1 {
+  font-size: 30px;
+  align-items: center;
+  color: #1E3551;
+  line-height: 52px;
+  font-weight: 100;
+  font-family: 'Great Vibes', cursive;
+}
+
+.manger{
+    font-family: papyrus;
+    font-size: 10px;
+    align-items: center;
+    color:rgba(0, 0, 0, 0.633);
+}
+
+.plats{
+    font-family: papyrus;
+    font-size: 20px;
+    align-items: center;
+    color:rgba(0, 0, 0, 0.633);
+}
+
+</style>
