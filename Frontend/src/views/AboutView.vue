a763479e47ceb0719d8b6713d334ada2f68af414
Zbram <75671598+Zbram@users.noreply.github.com>
2022-11-20 14:53:16 +0200

    change on footer

--- Frontend/src/views/AboutView.vue
+++ Frontend/src/views/AboutView.vue
@@ -1,5 +1,38 @@
 <template>
+  <MyHeader></MyHeader>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <video autoplay muted loop plays-inline id="myVideo" class="back-video">
+        <source src="@/assets/video/fond_manoir_sale.mp4" type="video/mp4">
+      </video>
+      <p class="BigText">"IN THE CENTRE OF THE PARISIAN GOLDEN TRIANGLE, A GASTRONOMIC RESTAURANT, NESTLED WITHIN A
+        SUPERB 19TH CENTURY PRIVATE MANSION."</p>
+    </div>
   </div>
 </template>
+
+<script>
+import MyHeader from '@/components/MyHeader.vue';
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.back-video {
+  position: absolute;
+  right: 0;
+  bottom: -15%;
+  z-index: -1;
+}
+</style>
